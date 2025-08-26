77141d305a7175426fb563716abd9eddce95d271
tunu5493 <maximilian.willer@uni-oldenburg.de>
2025-08-26 14:51:52 +0200

    Update and expend Start-Function

--- R/1_g__start_function__mwiller.R
+++ R/1_g__start_function__mwiller.R
@@ -18,6 +18,8 @@ flowengineR_start <- function() {
         paste(cli::symbol$record, "List available engines"),
         paste(cli::symbol$fancy_question_mark, "Help for run_workflow()"),
         paste(cli::symbol$star, "Show structure of control-object"),
+        paste(cli::symbol$info, "Open 'How to build custom Engines' vignette"),
+        paste(cli::symbol$info, "Open 'How to use LLM-Engine-Builder' vignette"),
         paste(cli::symbol$cross, "Exit")
       )
     )
@@ -51,6 +53,14 @@ flowengineR_start <- function() {
              control_example <- complete_control_with_defaults(list())
              assign("control_example", control_example, envir = .GlobalEnv)
              print(str(control_example))
+           },
+           {
+             message("\nOpening vignette 'How to build custom Engines'...\n")
+             print(utils::vignette("how_to_build_custom_engine"))
+           },
+           {
+             message("\nOpening vignette 'How to use LLM-Engine-Builder'...\n")
+             print(utils::vignette("how_to_use_llm_engine_builder"))
            },
            {
              message("\nGoodbye! Have fun with flowengineR!\n")
